2a052ed5a61b73cba1556fe55dca5d9712f09903
KhangTV02 <khangtv.work@gmail.com>
2024-12-30 14:21:30 +0700

    init

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+function App() {
+  return (
+    <div>
+      Hello World
+       <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
